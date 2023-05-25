c759e32db02338be935c09950193c90e1c92f1c7
SU_VIN <pyotnqls25@naver.com>
2023-05-25 15:34:04 +0900

    Ts 컴파일 옵션 설정

--- section2/app.js
+++ section2/app.js
@@ -1,36 +1,14 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-//위처럼 타입을 명시적으로 나타내는건 좋지않음
-//사용자 지정 타입
-//0부터 index가 차례대로 적용됨
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: "subin",
-    age: 24,
-    hobbies: ["Sports", "Cooking"],
-    role: Role.AUTHOR,
-};
-// let favoriteActivities: any[]; //데이터 타입 명시적 설정
-// //타입이 2개이상인 배열의 타입은 any
-// favoriteActivities = ["Sports", 1];
-if (person.role === Role.AUTHOR) {
-    console.log("hi author");
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 == "number" && typeof input2 == "number") {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
 }
-var favoriteActivities; //데이터 타입 명시적 설정
-favoriteActivities = ["Sports"];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toLowerCase());
-}
-//튜플
-var join;
-join = ["임수빈", "여자", 23];
-// join = ["임수빈",23,"여자"]; <-에러
+var combineAges = combine(30, 26);
+console.log(combineAges);
+var combineNames = combine("Max", "jenny");
+console.log(combineNames);
--- section2/app.ts
+++ section2/app.ts
@@ -1,43 +1,17 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
+//유니언 타임 => 매개변수를 보다 유연하게 사용할 수 있음
+function combine(input1: number | string, input2: number | string) {
+  let result;
 
-//위처럼 타입을 명시적으로 나타내는건 좋지않음
-
-//사용자 지정 타입
-//0부터 index가 차례대로 적용됨
-enum Role {
-  ADMIN,
-  READ_ONLY,
-  AUTHOR,
+  if (typeof input1 == "number" && typeof input2 == "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
 }
 
-const person = {
-  name: "subin",
-  age: 24,
-  hobbies: ["Sports", "Cooking"],
-  role: Role.AUTHOR,
-};
-// let favoriteActivities: any[]; //데이터 타입 명시적 설정
-// //타입이 2개이상인 배열의 타입은 any
-// favoriteActivities = ["Sports", 1];
+const combineAges = combine(30, 26);
+console.log(combineAges);
 
-if (person.role === Role.AUTHOR) {
-  console.log("hi author");
-}
-
-let favoriteActivities: String[]; //데이터 타입 명시적 설정
-
-favoriteActivities = ["Sports"];
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toLowerCase());
-}
-
-//튜플
-let join: [string, string, number];
-
-join = ["임수빈", "여자", 23];
-// join = ["임수빈",23,"여자"]; <-에러
+const combineNames = combine("Max", "jenny");
+console.log(combineNames);
